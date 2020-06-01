5ecaed2909f40617671eb73a052742a3db10be1a
G1Ryslan1G <Tukalovrd2@mail.ru>
2020-06-01 19:47:33 +0300

    add pages

---
+++ WpfApp1/WpfApp1/Navigations.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    public static class Navigations
+    {
+        private static List<Page> _pages = new List<Page>();
+        public static MainWindow main;
+
+        public static void NextPage(Page page)
+        {
+            _pages.Add(page);
+            main.FrameMain.Navigate(page);
+            main.LbNamePages.Content = page.Title;
+        }
+
+        public static void BackPage()
+        {
+            if (_pages.Count > 1)
+            {
+                _pages.RemoveAt(_pages.Count - 1);
+                main.LbNamePages.Content = _pages[_pages.Count - 1].Title;
+                main.FrameMain.GoBack();
+                if (_pages.Count == 1)
+                    main.BackBtn.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
